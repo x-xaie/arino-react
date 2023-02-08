cd1acdd7bb6360e24c6a88a15eedb34a58d7c97a
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 02:16:41 -0800

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -304,7 +304,7 @@ export default function Header({ variant }) {
           <Div className="cs-side_header_box">
             <Newsletter
               title="Subscribe"
-              subtitle="At vero eos et accusamus et iusto odio as part dignissimos ducimus qui blandit."
+              subtitle=""
               placeholder="example@gmail.com"
             />
           </Div>
