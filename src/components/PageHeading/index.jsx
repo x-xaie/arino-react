9c59ee472cc1ac9f0fbd141d39e2d31b33a4747c
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 03:45:09 -0800

    Update index.jsx

--- src/components/PageHeading/index.jsx
+++ src/components/PageHeading/index.jsx
@@ -6,7 +6,7 @@ export default function PageHeading({ title, bgSrc, pageLinkText }) {
   return (
     <Div>
       <Div className="container">
-        <Div className="cs-page_heading_in">
+
           <h1 className="cs-page_title cs-font_50 cs-white_color">{title}</h1>
           <ol className="breadcrumb text-uppercase">
             <li className="breadcrumb-item">
@@ -15,7 +15,6 @@ export default function PageHeading({ title, bgSrc, pageLinkText }) {
             <li className="breadcrumb-item active">{pageLinkText}</li>
           </ol>
         </Div>
-      </Div>
     </Div>
   );
 }
