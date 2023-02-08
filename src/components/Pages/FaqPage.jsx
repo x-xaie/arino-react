afd90ab73fe0e92d103f483fb1895a4fe0118f5b
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 04:19:11 -0800

    Update FaqPage.jsx

--- src/components/Pages/FaqPage.jsx
+++ src/components/Pages/FaqPage.jsx
@@ -21,7 +21,7 @@ export default function FaqPage() {
       <Div className="container">
         <Div className="row">
           <Div className="col-lg-4">
-            <Div bgSrc="/images/faq.pmg">
+            <Div bgSrc="/images/faq.png">
               <Div className="cs-height_30 cs-height_lg_30" />
               <ul className="cs-list cs-style1 cs-mp0">
 
