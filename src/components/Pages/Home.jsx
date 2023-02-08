e66c8cabc8c96b8887f7c151ef38691be20dd18f
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 02:17:54 -0800

    Update Home.jsx

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -213,7 +213,7 @@ export default function Home() {
       {/* Start CTA Section */}
       <Div className="container">
         <Cta
-          title="Let’s disscuse make <br />something <i>cool</i> together"
+          title="Let’s disscuse make <br />partner with the <i>cool</i> Future of Report Generation"
           btnText="Apply For Meeting"
           btnLink="/contact"
           bgSrc="/images/cta_bg.jpeg"
