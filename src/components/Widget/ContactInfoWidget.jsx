2dec3110a8d751741b1c2b257d33f01dfd38749d
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 00:59:13 -0800

    Update ContactInfoWidget.jsx

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -8,7 +8,7 @@ export default function ContactInfoWidget({withIcon, title}) {
       <ul className="cs-menu_widget cs-style1 cs-mp0">
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="material-symbols:add-call-rounded" /></span>:''}
-          +44 454 7800 112
+          +1 647 574 6999
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:envelope" /></span>:''}
@@ -16,7 +16,7 @@ export default function ContactInfoWidget({withIcon, title}) {
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:map-marker" /></span>:''}
-          50 Wall Street Suite, 44150 <br/>Ohio, United States
+          2A Church St, M5E 0E1 <br/>Toronto, Canada
         </li>
       </ul>
     </>
