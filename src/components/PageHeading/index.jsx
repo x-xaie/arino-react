2548315e4fdfdaa036192d69bb35157535e36a66
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-08 03:47:07 -0800

    Update index.jsx

--- src/components/PageHeading/index.jsx
+++ src/components/PageHeading/index.jsx
@@ -9,9 +9,6 @@ export default function PageHeading({ title, bgSrc, pageLinkText }) {
 
           <h1 className="cs-page_title cs-font_50 cs-white_color">{title}</h1>
           <ol className="breadcrumb text-uppercase">
-            <li className="breadcrumb-item">
-              <Link to="/">Home</Link>
-            </li>
             <li className="breadcrumb-item active">{pageLinkText}</li>
           </ol>
         </Div>
