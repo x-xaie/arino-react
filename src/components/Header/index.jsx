82edf73421776c96d29684af1d2f905a420a4d94
x-xaie <122314770+x-xaie@users.noreply.github.com>
2023-02-08 23:01:35 -0800

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -31,7 +31,7 @@ export default function Header({ variant }) {
             <Div className="cs-main_header_in">
               <Div className="cs-main_header_left">
                 <Link className="cs-site_branding" to="/">
-                  <img src="/images/logo.svg" alt="Logo" width="270px" />
+                  <img src="/images/logoooo.svg" alt="Logo" />
                 </Link>
               </Div>
               <Div className="cs-main_header_center">
@@ -127,7 +127,7 @@ export default function Header({ variant }) {
         <Div className="cs-side_header_in">
           <Div className="cs-side_header_shape" />
           <Link className="cs-site_branding" to="/">
-            <img src="/images/logo.svg" alt="Logo" width="590px"/>
+            <img src="/images/logoooo.svg" alt="Logo"/>
           </Link>
           <Div className="cs-side_header_box">
             <h2 className="cs-side_header_heading">
